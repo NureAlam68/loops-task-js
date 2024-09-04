8685331a3c404a89e332b3b1df479069cc89cb5a
NureAlam68 <nurealam151068@gmail.com>
2024-09-04 17:35:07 +0600

    multiplication table for number 9

---
+++ multiplication-table.js
@@ -0,0 +1,19 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+
+
+
+const number = 9;
+
+
+for(let i = 1; i <= 10; i++) {
+
+    const result = i * number;
+
+    console.log(`${number} * ${i} = ${result}`);
+}
