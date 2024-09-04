2b9b7557602228ccc98867b2cfdf41975448afa6
NureAlam68 <nurealam151068@gmail.com>
2024-09-04 17:16:55 +0600

    sum of even and odd number

---
+++ sum.js
@@ -0,0 +1,40 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+// let sum = 0;
+
+// for(let i = 61; i <= 100; i++) {
+//     if(i % 2 === 1) {
+//         sum = sum + i;
+//     }
+// }
+// console.log('Sum of odd numbers:', sum);
+
+
+
+
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+
+let sum = 0;
+
+for(let i = 78; i <= 98; i++) {
+    if(i % 2 === 0) {
+        sum = sum + i;
+    }
+}
+// console.log('Sum of odd numbers:', sum);
