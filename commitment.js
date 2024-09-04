0651459c078fb2da85d1a79ff9c02492ce574c69
NureAlam68 <nurealam151068@gmail.com>
2024-09-04 16:45:30 +0600

    loop task js

---
+++ commitment.js
@@ -0,0 +1,8 @@
+// task- "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+
+let commitment = 'I will invest at least 6 hrs every single day for next 60 days!';
+
+for(let i = 1; i <= 60; i++) {
+    console.log(commitment);
+}
