7efd07ad664b1585d581952d00334034b0037205
NureAlam68 <nurealam151068@gmail.com>
2024-09-05 10:20:40 +0600

    break js

---
+++ break.js
@@ -0,0 +1,28 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+// for loop
+
+// for(let i = 1; i <= 200; i++) {
+//     console.log(i);
+//     if(i >= 100) {
+//         break;
+//     }
+// }
+
+
+// while loop
+
+
+let i = 1;
+
+while(i <= 200) {
+    console.log(i);
+    if(i >= 100) {
+        break;
+    }
+    i++;
+}
