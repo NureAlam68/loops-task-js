077ce42980b762204f11e6b1398f7c61d671fed6
NureAlam68 <nurealam151068@gmail.com>
2024-09-05 09:54:12 +0600

    countdown using while loop

--- countdown.js
+++ countdown.js
@@ -7,13 +7,48 @@ Implement a countdown timer that counts down from 81 to 65.
 
 
 
+// for(i = 81; i >=65; i--) {
+//     console.log(i);
+// }
 
-let count = 82;
-const timer = setInterval(function() {
-    count--;
-    console.log(count);
-    if (count === 65) {
-    clearInterval(timer);
-    console.log("Time's up!");
-    }
-}, 1000);
+
+
+
+
+// time countdown
+
+// let count = 82;
+// const timer = setInterval(function() {
+//     count--;
+//     console.log(count);
+//     if (count === 65) {
+//     clearInterval(timer);
+//     console.log("Time's up!");
+//     }
+// }, 1000);
+
+
+
+
+
+
+// while loop
+
+
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+
+
+let i = 21;
+
+while(i >= 15) {
+    console.log(i);
+    i--;
+}
+
+
+
