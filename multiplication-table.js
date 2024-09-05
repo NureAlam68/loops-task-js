20e6edf3f34886aeb48b22c34e318733dd66cf8e
NureAlam68 <nurealam151068@gmail.com>
2024-09-05 09:39:34 +0600

    multiplication table using while loop

--- multiplication-table.js
+++ multiplication-table.js
@@ -8,12 +8,37 @@ Generate a multiplication table for number 9
 
 
 
-const number = 9;
+// const number = 9;
 
 
-for(let i = 1; i <= 10; i++) {
+// for(let i = 1; i <= 10; i++) {
 
+//     const result = i * number;
+
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+
+
+
+// while loop
+
+
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+
+const number = 5;
+
+let i = 1;
+
+while(i <= 10) {
     const result = i * number;
-
     console.log(`${number} * ${i} = ${result}`);
+    i++;
 }
