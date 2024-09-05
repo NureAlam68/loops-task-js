184855b8b095f2f1eef0e25e4ba1872cd9eaccc9
NureAlam68 <nurealam151068@gmail.com>
2024-09-05 09:32:53 +0600

    even odd nember sum using while loop

--- sum.js
+++ sum.js
@@ -7,14 +7,14 @@ Find all the odd numbers from 61 to 100.
  */
 
 
-// let sum = 0;
+let sum = 0;
 
-// for(let i = 61; i <= 100; i++) {
-//     if(i % 2 === 1) {
-//         sum = sum + i;
-//     }
-// }
-// console.log('Sum of odd numbers:', sum);
+for(let i = 61; i <= 100; i++) {
+    if(i % 2 === 1) {
+        sum = sum + i;
+    }
+}
+console.log('Sum of odd numbers:', sum);
 
 
 
@@ -30,11 +30,69 @@ Find all the even numbers from 78 to 98.
  */
 
 
-let sum = 0;
+// let sum = 0;
 
-for(let i = 78; i <= 98; i++) {
-    if(i % 2 === 0) {
-        sum = sum + i;
-    }
-}
-console.log('Sum of odd numbers:', sum);
+// for(let i = 78; i <= 98; i++) {
+//     if(i % 2 === 0) {
+//         sum = sum + i;
+//     }
+// }
+// console.log('Sum of even numbers:', sum);
+
+
+
+
+
+
+// while loop
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+
+
+
+// let i = 81;
+// let sum = 0;
+
+// while(i <= 131) {
+//     if(i % 2 === 1) {
+//         sum = sum + i;
+//     }
+//     i++;
+// }
+// console.log('Sum of odd numbers:', sum);
+
+
+
+
+
+
+
+
+
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+
+
+// let i = 206;
+// let sum = 0;
+
+// while(i <= 311) {
+//     if(i % 2 === 0) {
+//         sum = sum + i;
+//     }
+//     i++;
+// }
+// console.log('sum of even numbers:', sum);
